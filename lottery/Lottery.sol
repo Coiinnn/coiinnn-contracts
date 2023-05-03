57df630a4419faa2b86fbdcd47cefc9c76490c08
odiinnn <msinkevic103@gmail.com>
2023-05-03 21:31:15 +0300

    add coiinnn storage address and commission

--- lottery/Lottery.sol
+++ lottery/Lottery.sol
@@ -2,14 +2,18 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
+
+
 contract Lottery {
 
     address private owner;
+    address private coiinnnStorage;
     uint private randNonce = 0;
     uint private modulus = 10;
     uint16 private winPercent;
 
     event OwnerSet(address indexed oldOwner, address indexed newOwner);
+    event StorageSet(address indexed oldStorage, address indexed newStorage);
     event StartGame(address player, uint256 amount);
     event LoseGame(address player, uint256 amount, uint number);
     event WinGame(address player, uint256 amount, uint number);
@@ -23,8 +27,10 @@ contract Lottery {
 
     constructor() {
         owner = msg.sender;
+        coiinnnStorage = 0x0ECFEd46f2B4C67F6DEC9C08fD888c5EAFC0685B;
         winPercent = 5;
         emit OwnerSet(address(0), owner);
+        emit StorageSet(address(0), 0x0ECFEd46f2B4C67F6DEC9C08fD888c5EAFC0685B);
     }
 
     function changeOwner(address newOwner) public isOwner {
@@ -32,8 +38,13 @@ contract Lottery {
         owner = newOwner;
     }
 
+    function changeCoiinnnStorage(address newCoiinnnStorage) public isOwner {
+        emit StorageSet(coiinnnStorage, newCoiinnnStorage);
+        coiinnnStorage = newCoiinnnStorage;
+    }
+
     function changeWinPercent(uint16 newWinPercent) external isOwner {
-        require(newWinPercent < 10 && newWinPercent > 0, "New win percent should be in ranhe 1% - 99%");
+        require(newWinPercent < 10 && newWinPercent > 0, "New win percent should be in range 1% - 99%");
         emit ChangeWinPercent(msg.sender, newWinPercent);
         winPercent = newWinPercent;
     }
@@ -44,6 +55,10 @@ contract Lottery {
 
     function getWinPercent() external view returns (uint16) {
         return winPercent * 10;
+    }
+
+    function getCoiinnnStorage() external view returns (address) {
+        return coiinnnStorage;
     }
 
     function topUpBalance() external payable {
@@ -60,15 +75,17 @@ contract Lottery {
             emit LoseGame(msg.sender, msg.value, randomValue);
         } else {
             emit WinGame(msg.sender, msg.value, randomValue);
-            (bool os, ) = payable(msg.sender).call{value: msg.value * 2}("");
-            require(os);
+            (bool hs, ) = payable(coiinnnStorage).call{value: msg.value * 5 / 100}("");
+            (bool os, ) = payable(msg.sender).call{value: msg.value * 195 / 100}("");
+            require(os, "Failed to send your reward");
+            require(hs, "Failed to send commission");
         }
 
     }
 
     function random() private returns (uint) {
         randNonce++;
-        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % modulus + 1;
+        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, block.prevrandao, randNonce))) % modulus + 1;
     }
 
 }
